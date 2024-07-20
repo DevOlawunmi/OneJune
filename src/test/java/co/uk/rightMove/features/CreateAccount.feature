063cd84c8a48d950f0584af08a2100457b36a77e
HP <olawunmi263@yahoo.com>
2024-07-20 05:44:34 +0100

    Create account feature updated

--- src/test/java/co/uk/rightMove/features/CreateAccount.feature
+++ src/test/java/co/uk/rightMove/features/CreateAccount.feature
@@ -2,3 +2,6 @@ Feature: Search Property For Sale
   Scenario: User can successfully search for property for sale
     Given I navigate to the homepage
     And I click accept cookies
+    And I click on the sign in or create account button
+    And I enter email address as "Ola@gmail.com"
+    And I click on the continue button
---
+++ src/test/java/co/uk/rightMove/stepDefs/CreateAcctStepDefs.java
@@ -0,0 +1,21 @@
+package co.uk.rightMove.stepDefs;
+
+import co.uk.rightMove.pages.BasePage;
+import io.cucumber.java.en.Given;
+
+public class CreateAcctStepDefs extends BasePage {
+    @Given("I click on the sign in or create account button")
+    public void i_click_on_the_sign_in_or_create_account_button() {
+
+    }
+
+    @Given("I enter email address as {string}")
+    public void i_enter_email_address_as(String string) {
+
+    }
+
+    @Given("I click on the continue button")
+    public void i_click_on_the_continue_button() {
+
+    }
+}
